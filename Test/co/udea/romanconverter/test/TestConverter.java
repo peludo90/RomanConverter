220ee0f31f0bdc8ce4b78275e047a7b41ba22a77
peludo90 <oagv90@hotmail.com>
2014-11-28 17:34:28 -0500

    Correción de errores y pruebas completas
    Se termina de agregar todas las pruebas para muchos más casos generados
    por un error encontrados en el case, el cual fue corregido

--- Test/co/udea/romanconverter/test/TestConverter.java
+++ Test/co/udea/romanconverter/test/TestConverter.java
@@ -79,7 +79,7 @@ public class TestConverter {
 	public void convert99(){
 		Converter converter = new Converter();
 		
-		String intConverted = converter.toRoman(33);
+		String intConverted = converter.toRoman(99);
 		
 		assertEquals("XCIX",intConverted);	
 	}
@@ -114,9 +114,10 @@ public class TestConverter {
 	
 	@Test
 	public void convert1853(){
+
 		Converter converter = new Converter();
 		
-		String intConverted = converter.toRoman(1000);
+		String intConverted = converter.toRoman(1853);
 		
 		assertEquals("MDCCCLIII",intConverted);	
 	}
@@ -126,17 +127,116 @@ public class TestConverter {
 	public void convert3999(){
 		Converter converter = new Converter();
 		
-		String intConverted = converter.toRoman(1000);
+		String intConverted = converter.toRoman(3999);
 		
 		assertEquals("MMMCMXCIX",intConverted);	
 	}
 	
 	@Test
 	public void convert4000(){
+
 		Converter converter = new Converter();
 		
-		String intConverted = converter.toRoman(1000);
+		String intConverted = converter.toRoman(4000);
 		
 		assertEquals(null,intConverted);	
 	}
+	
+	@Test
+	public void convertMinusOne(){
+		Converter converter = new Converter();
+		
+		String intConverted = converter.toRoman(-1);
+		
+		assertEquals(null,intConverted);	
+	}
+	
+	@Test
+	public void convertZero(){
+		Converter converter = new Converter();
+		
+		String intConverted = converter.toRoman(0);
+		
+		assertEquals(null,intConverted);	
+	}
+	
+	@Test
+	public void convert801(){
+		Converter converter = new Converter();
+		
+		String intConverted = converter.toRoman(801);
+		
+		assertEquals("DCCCI",intConverted);	
+	}
+	
+	@Test
+	public void convert451(){
+
+		Converter converter = new Converter();
+		
+		String intConverted = converter.toRoman(451);
+		
+		assertEquals("CDLI",intConverted);	
+	}
+	
+	@Test
+	public void convert500(){
+
+		Converter converter = new Converter();
+		
+		String intConverted = converter.toRoman(500);
+		
+		assertEquals("D",intConverted);	
+	}
+
+	@Test
+	public void convert600(){
+		Converter converter = new Converter();
+		
+		String intConverted = converter.toRoman(600);
+		
+		assertEquals("DC",intConverted);	
+	}
+
+	@Test
+	public void convert599(){
+
+		Converter converter = new Converter();
+		
+		String intConverted = converter.toRoman(599);
+		
+		assertEquals("DXCIX",intConverted);	
+	}
+	
+
+	@Test
+	public void convert702(){
+
+		Converter converter = new Converter();
+		
+		String intConverted = converter.toRoman(702);
+		
+		assertEquals("DCCII",intConverted);	
+	}
+	
+	@Test
+	public void convert800(){
+
+		Converter converter = new Converter();
+		
+		String intConverted = converter.toRoman(800);
+		
+		assertEquals("DCCC",intConverted);	
+	}
+	
+	@Test
+	public void convert80(){
+
+		Converter converter = new Converter();
+		
+		String intConverted = converter.toRoman(80);
+		
+		assertEquals("LXXX",intConverted);	
+	}
+	
 }
--- src/co/udea/romarconverter/entity/Converter.java
+++ src/co/udea/romarconverter/entity/Converter.java
@@ -2,73 +2,78 @@ package co.udea.romarconverter.entity;
 
 public class Converter {
 
-	String[][] bases = {{"I","V","X"},
-						{"X","L","C"},
-						{"C","D","M"}};
-	
-	public Converter(){
+	String[][] bases = { { "I", "V", "X" }, { "X", "L", "C" },
+			{ "C", "D", "M" }, { "M", "", "" } };
+
+	public Converter() {
 		super();
 	}
-	
-	public String toRoman(int value){
-		
-		
-		if(isInRange(value)){
-		String result = "";
-		int exponent = valueSize(value);
-		do{ 
-			exponent--;
-			int alfa =(int) (value/Math.pow(10,exponent));
-			value = (int) (value%Math.pow(10,exponent));
-			result +=	 convertModule(alfa,bases[exponent][0],bases[exponent][1],bases[exponent][2]);
-			
-		}while(value!= 0);
-		
-		return result;
-		}else{
+
+	public String toRoman(int value) {
+
+		if (isInRange(value)) {
+			String result = "";
+			int exponent = valueSize(value);
+			do {
+				exponent--;
+				// se descompone el número en la unidad mayor al dividirla por
+				// 10 elevado a un exponente correspondiente
+				// ejm 1324 / 1000 
+				int alfa = (int) (value / Math.pow(10, exponent));
+				// y el sobrante de la división se deja para el siguiente ciclo
+				//ejem 1324 %1000 = 324
+				value = (int) (value % Math.pow(10, exponent));
+				result += convertModule(alfa, bases[exponent][0],
+						bases[exponent][1], bases[exponent][2]);
+
+				// el valor ha de llegar a 0, puesto q al final no sobrará nada
+			} while (value != 0);
+
+			return result;
+		} else {
 			return null;
 		}
 	}
-	
-	
-	
-	
-	
-	public String convertModule(int value, String baseMono, String basePenta, String baseDeca){
+
+	public String convertModule(int value, String baseMono, String basePenta,
+			String baseDeca) {
+		// Todos los números romanos se pueden escribir en funcion de base 1,5 y
+		// 10
 		switch (value) {
 		case 1:
 			return baseMono;
 		case 2:
-			return baseMono+baseMono;
+			return baseMono + baseMono;
 		case 3:
-			return baseMono+baseMono+baseMono;
+			return baseMono + baseMono + baseMono;
 		case 4:
-			return baseMono+basePenta;
+			return baseMono + basePenta;
 		case 5:
 			return basePenta;
 		case 6:
-			return basePenta+baseMono;
+			return basePenta + baseMono;
 		case 7:
-			return basePenta+baseMono+baseMono;
+			return basePenta + baseMono + baseMono;
 		case 8:
-			return baseMono+baseMono+baseMono+baseMono;
+			return basePenta + baseMono + baseMono + baseMono;
 		case 9:
-			return baseMono+baseDeca;
+			return baseMono + baseDeca;
 		default:
-			return null;
+			return "";
 		}
 
 	}
-	
-	public int valueSize(int value){
+
+	public int valueSize(int value) {
+		//método para obtener el número de dígitos
 		return String.valueOf(value).length();
 	}
-	
-	
-	public boolean isInRange(int value){
-		if(0<value&&value<4000){
+
+	public boolean isInRange(int value) {
+		//método para verificar que este dentro de los números que se pueden representar
+		if (0 < value && value < 4000) {
 			return true;
-		}else{
+		} else {
 			return false;
 		}
 	}
